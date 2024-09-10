69efb73c5fc7dcf8132f0eb0714f57be058e9acf
meghanakurapati123 <137704119+meghanakurapati123@users.noreply.github.com>
2024-09-09 20:00:07 -0400

    Add files via upload

---
+++ Twimba/public/data.js
@@ -0,0 +1,57 @@
+export const tweetsData = [   
+    {
+        handle: `@CryptoGuru1234567`,
+        profilePic: `images/crypto.jpg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Invest in Dogecoin, SHIBA for massive gains! 🚀🚀 Check DMs for secret tips!`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: '@JeffBezos ✅',
+        profilePic: `images/bezos.jpg`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `Looking for brave souls to journey to the moon 🌕. No passport required! 🛰️`,
+        replies: [
+                  {
+                handle: `@DwayneJohnson ✅`,
+                profilePic: `images/johnson.webp`,
+                tweetText: `Count me in! 🌋💪`,
+            },
+                  {
+                handle: `@KeanuReeves ✅`,
+                profilePic: `images/reeves.jpg`,
+                tweetText: `Already packed my bags. 🚴‍♂️`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@CodeNinja99`,
+        profilePic: `images/emoji.jpeg`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `Is knowing HTML&CSS enough to call yourself a developer?`,
+        replies: [
+            {
+                handle: `@CodeMasterPro`,
+                profilePic: `images/stack.jpg`,
+                tweetText: `Absolutely not. Get back to basics!`,
+            },
+            {
+                handle: `@PositiveVibesOnly`,
+                profilePic: `images/vibes.jpg`,
+                tweetText: `You are amazing just the way you are! 💖`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
